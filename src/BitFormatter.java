5383ce33a56b42755fa108e8069cf163ff3036d6
Alexey Sytchev <a.sytchev@outlook.com>
2015-02-21 22:10:47 +0300

    Refactoring formatting function

--- src/BitFormatter.java
+++ src/BitFormatter.java
@@ -4,51 +4,55 @@ import java.util.Arrays;
  * Created by Alexey Sychev on 21.02.2015.
  */
 public class BitFormatter {
-    static final int INT_LENGTH = 32;
-    static final int LONG_LENGTH = 64;
 
-    public static String toFormatedBinaryString(int number) {
-        StringBuilder sb = new StringBuilder();
+    public static String toFormattedBinaryString(byte number) {
+
         char[] symbols = Integer.toBinaryString(number).toCharArray();
-
-        char[] resultArray = new char[INT_LENGTH];
-        int delta = resultArray.length - symbols.length;
-        Arrays.fill(resultArray, 0, delta, '0');
-        for(int i = 0; i < symbols.length; i++) {
-            resultArray[(resultArray.length-1) - i] = symbols[(symbols.length-1) -i];
-        }
-
-        System.out.println(number + " ->");
-        for(int i=0; i< resultArray.length; i++) {
-            sb.append(resultArray[i]);
-            if((i+1) % 8 == 0) {
-                sb.append(' ');
-            }
-        }
-        return sb.toString();
+        return makeString(symbols, Byte.SIZE);
 
     }
 
-    public static String toFormatedBinaryString(long number) {
+    public static String toFormattedBinaryString(short number) {
+
+        char[] symbols = Integer.toBinaryString(number).toCharArray();
+        return makeString(symbols, Short.SIZE);
+
+    }
+
+    public static String toFormattedBinaryString(int number) {
+
+        char[] symbols = Integer.toBinaryString(number).toCharArray();
+        return makeString(symbols, Integer.SIZE);
+
+    }
+
+    public static String toFormattedBinaryString(long number) {
+
+        char[] symbols = Long.toBinaryString(number).toCharArray();
+        return makeString(symbols, Long.SIZE);
+
+    }
+
+    private static String makeString(char[] symbols, int sizeOfType ) {
 
         StringBuilder sb = new StringBuilder();
-        char[] symbols = Long.toBinaryString(number).toCharArray();
+        char[] resultArray = new char[sizeOfType];
 
-        char[] resultArray = new char[LONG_LENGTH];
         int delta = resultArray.length - symbols.length;
         Arrays.fill(resultArray, 0, delta, '0');
+
         for(int i = 0; i < symbols.length; i++) {
-            resultArray[(resultArray.length-1) - i] = symbols[(symbols.length-1) -i];
+            resultArray[delta + i] = symbols[i];
         }
 
-        System.out.println(number + " ->");
         for(int i=0; i< resultArray.length; i++) {
             sb.append(resultArray[i]);
-            if((i+1) % 8 == 0) {
+            if((i+1) % Byte.SIZE == 0) {
                 sb.append(' ');
             }
         }
-        return sb.toString();
 
+        return sb.toString();
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(BitFormatter.toFormatedBinaryString(1));
-        System.out.println(BitFormatter.toFormatedBinaryString(Long.MAX_VALUE));
+        System.out.println(BitFormatter.toFormattedBinaryString((byte) 1));
+        System.out.println(BitFormatter.toFormattedBinaryString((byte) 2));
+        System.out.println(BitFormatter.toFormattedBinaryString((byte) Byte.MAX_VALUE));
+        System.out.println(BitFormatter.toFormattedBinaryString((short) 1));
+        System.out.println(BitFormatter.toFormattedBinaryString((short) 2));
+        System.out.println(BitFormatter.toFormattedBinaryString((short) Short.MAX_VALUE));
+        System.out.println(BitFormatter.toFormattedBinaryString(1));
+        System.out.println(BitFormatter.toFormattedBinaryString(2));
+        System.out.println(BitFormatter.toFormattedBinaryString(Integer.MAX_VALUE));
+        System.out.println(BitFormatter.toFormattedBinaryString(1L));
+        System.out.println(BitFormatter.toFormattedBinaryString(2L));
+        System.out.println(BitFormatter.toFormattedBinaryString(Long.MAX_VALUE));
+
+
     }
 
 }
